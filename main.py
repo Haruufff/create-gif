9853082c7a981eb111518ed427bd21c4919193bf
Haruuff <hafidhmaruf231@gmail.com>
2025-08-21 11:43:29 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+'''this program serve as the final project, focusing on the creation a GIF image'''
+
+import imageio.v3 as iio
+
+def gif_animation(pic_1, pic_2, pic_3, pic_4):
+    '''A function to process a collection of images into a GIF'''
+    
+    filenames = [pic_1, pic_2, pic_3, pic_4]
+    images = []
+
+    for filenames in filenames:
+        images.append(iio.imread(filenames))
+
+    iio.imwrite('result_gif.gif', images, duration=500, loop=0)
+
+gif_animation("dino1.png", "dino2.png", "dino3.png", "dino4.png")
